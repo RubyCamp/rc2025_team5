a170f604ebbb864116c249efbb37e09acb3b01e4
mini <mini.mini.08neko@gmail.com>
2025-08-26 07:51:34 +0000

    business_hour

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_23_085142) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_26_062845) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -50,6 +50,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_23_085142) do
     t.string "tags"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.time "open"
+    t.time "close"
   end
 
   create_table "reviews", force: :cascade do |t|
