9f74aaecc76a6d0858e0c1133417255e87874bb9
Furuta3 <urma524chihi@gmail.com>
2025-08-28 06:00:59 +0000

    イベントのカラム追加マイグレーションファイルの修正

--- db/migrate/20250828014136_addtags_to_events.rb
+++
@@ -1,7 +0,0 @@
-class AddtagsToEvents < ActiveRecord::Migration[8.0]
-  def change
-    add_column :events, :tags, :string
-    add_column :events, :open, :time
-    add_column :events, :close, :time
-  end
-end
