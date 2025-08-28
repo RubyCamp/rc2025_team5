790b61d67006601477259fc20e6b15b27bfc1956
Furuta3 <urma524chihi@gmail.com>
2025-08-28 05:16:30 +0000

    イベント、夜景、飲食店モデルにimagesの関連とバリデーションを追加

--- app/models/night_spot.rb
+++ app/models/night_spot.rb
@@ -1,3 +1,8 @@
 class NightSpot < ApplicationRecord
   validates :name, presence: true
+  has_many_attached :images
+  validates :images,
+  content_type: [ "image/jpeg", "image/png", "image/gif" ], # 許可ファイル形式
+  size: { less_than: 5.megabytes },                       # 1枚あたり5MB未満
+  limit: { max: 3 }
 end
